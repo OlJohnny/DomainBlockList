dc3440a5871fb650ce3a7bb96f00029713f90859
OlJohnny <Klorkson.NoksuLP@gmail.com>
2019-03-18 19:57:33 +0100

    fixed some shit

--- git-get-blocklist.sh
+++ git-get-blocklist.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo wget -O /etc/pihole/regex.list https://raw.githubusercontent.com/OlJohnny/DomainBlockList/master/Blocklist
+sudo wget -O /etc/pihole/regex.list https://raw.githubusercontent.com/OlJohnny/DomainBlockList/master/regex-blacklist
 sudo chmod -Rf 775 /etc/pihole/regex.list && sudo chown -Rf pihole:www-data /etc/pihole/regex.list
 sudo service pihole-FTL reload
--- make-blocklist.sh
+++ make-blocklist.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-rm -rf ./blocklist-work
+
+sudo rm -rf ./blocklist-work
+sudo rm -rf ./blocklist-fin
+
+sudo touch ./blocklist-work
+sudo touch ./blocklist-fin
+
+sudo chmod -f 775 ./blocklist-work
+sudo chmod -f 775 ./blocklist-fin
+
+sudo wget -i ./blocklist-links -O - >> ./blocklist-work
+#-q                      --quiet                                Verhindert dass wget Informationen auf der Konsole ausgibt.
+# -i DATEI      --input-file=DATEI              Liest URLs aus einer Text- oder HTML-Datei aus.
+# -O DATEI      --output-document=DATEI Schreibe in DATEI. Diese Option kann nicht mit der Option -N zusammen verwendet werden. Gibt man -O "-" an, so schreibt wget den Inhalt $
 
 
-wget -q -i ./blocklists-source -O "-" >> ./blocklist-work
-#-q			 --quiet				Verhindert dass wget Informationen auf der Konsole ausgibt.
-# -i DATEI	--input-file=DATEI		Liest URLs aus einer Text- oder HTML-Datei aus.
-# -O DATEI	--output-document=DATEI	Schreibe in DATEI. Diese Option kann nicht mit der Option -N zusammen verwendet werden. Gibt man -O "-" an, so schreibt wget den Inhalt der heruntergeladenen Datei direkt auf die Standardausgabe (stdout) des Terminals.
-
-
-grep -v -f ./RegEx-Blacklist ./blocklist-work
-# -v		--invert-match			Invertiert die Suche und liefert alle Zeilen die nicht auf das gesuchte Muster passen.
-# -f Datei	--file=Datei			beziehe die Muster aus Datei, eines je Zeile. Eine leere Datei enthält keine Muster und passt somit auf keinen String.
+sudo grep -E -v -f ./regex-blacklist ./blocklist-work > ./blocklist-fin
+# -v            --invert-match                  Invertiert die Suche und liefert alle Zeilen die nicht auf das gesuchte Muster passen.
+# -f Datei      --file=Datei                    beziehe die Muster aus Datei, eines je Zeile. Eine leere Datei enthält keine Muster und passt somit auf keinen String.
