1cfe5a129778a511aa1ed2678444cbac4d5a2f25
OlJohnny <Klorkson.NoksuLP@gmail.com>
2019-03-18 18:18:52 +0100

    changed script, so that it actually works

--- git-get-blocklist.sh
+++ git-get-blocklist.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-rm -rf /etc/pihole/regex_temp/
-sudo git clone git@github.com:OlJohnny/DomainBlockList.git /etc/pihole/regex_temp/
-rm -rf /etc/pihole/regex.list
-mv -f /etc/pihole/regex_temp/blocklist /etc/pihole/regex.list
-rm -rf /etc/pihole/regex_temp/
+sudo wget -O /etc/pihole/regex.list https://raw.githubusercontent.com/OlJohnny/DomainBlockList/master/Blocklist
 sudo chmod -Rf 775 /etc/pihole/regex.list && sudo chown -Rf pihole:www-data /etc/pihole/regex.list
