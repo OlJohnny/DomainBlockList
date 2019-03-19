4aa8168a6a582510df0bdb4c13297d289f5ab1ed
OlJohnny <Klorkson.NoksuLP@gmail.com>
2019-03-19 17:56:39 +0100

    General Upgrade (Better script, README, blacklist, blocklists)

--- README.md
+++ README.md
@@ -4,6 +4,17 @@ This ``Blocklist`` is intended to be used with PiHole or any other Domain Blocke
 ``git-get-domainblockList-regex.sh`` is a script which clones the latest version of the ``Blocklist`` and replaces the existing ``/etc/pihole/regex.list`` from pihole.
 It is recommended to put this script into ``cron.daily`` to keep the ``Blocklist`` up-to-date.
 
+
+## How To Use The Scripts
+### git-get-domainblockList-regex.sh
+Run ``chmod +x git-get-domainblockList-regex.sh`` to make it executeable and ``dos2unix git-get-domainblockList-regex.sh`` to convert any Windows specific symbols, line breaks, etc.
+This script will automatically get the current ``regex-blacklist`` and put it in place for pihole to use.
+
+### make-blocklist.sh
+Run ``chmod +x make-blocklist.sh`` to make it executeable and ``dos2unix make-blocklist.sh`` to convert any Windows specific symbols, line breaks, etc.
+You may also add write permissions for ``blocklist-fin``, ``blocklist-links`` or ``regex-blacklist``.
+This script will get blocklists (the links of which can be added in ``blocklist-links``) and give out all Domains, which do NOT match a given set of RegEx statements (can be edited in ``regex-blacklist``)
+
 ## Whitelisting
 As this list aims to block most tracking and ad services you may need to specifically whitelist the following domains, as they may :
 - (www.)facebook.com *to use Facebook*
--- make-blocklist.sh
+++ make-blocklist.sh
@@ -9,10 +9,21 @@ sudo touch ./blocklist-fin
 sudo chmod -f 775 ./blocklist-work
 sudo chmod -f 775 ./blocklist-fin
 
-sudo wget -i ./blocklist-links -O - >> ./blocklist-work
+
+now=$(date +"%T")
+echo "<$now> Started  Getting Blocklists..."
+
+sudo wget -q -i ./blocklist-links -O - >> ./blocklist-work
 #-q             --quiet                 Verhindert dass wget Informationen auf der Konsole ausgibt.
 # -i DATEI      --input-file=DATEI      Liest URLs aus einer Text- oder HTML-Datei aus.
 # -O DATEI      --output-document=DATEI Schreibe in DATEI. Kann nicht mit -N verwendet werden. Mit "-O -" schreibt wget den Inhalt der heruntergeladenen Datei in die StdOut.
+
+now=$(date +"%T")
+echo "<$now> Finished Getting Blocklists"
+
+
+now=$(date +"%T")
+echo "<$now> Started  Sorting and Cleaning Blocklist..."
 
 sudo sed -i 's/0.0.0.0//' ./blocklist-work
 sudo sed -i 's/127.0.0.1//' ./blocklist-work
@@ -24,10 +35,22 @@ sudo sed -i 's/[[:space:]]//g' ./blocklist-work
 sudo sort -u ./blocklist-work -o ./blocklist-work1
 #-u             --unique                Sortierung ohne doppelte Zeilen
 
+now=$(date +"%T")
+echo "<$now> Finished Sorting Blocklist"
+
+
 sudo mv ./blocklist-work1 ./blocklist-work
+
+
+now=$(date +"%T")
+echo "<$now> Started  Applying RegEx Blacklist..."
 
 sudo grep -E -v -f ./regex-blacklist ./blocklist-work > ./blocklist-fin
 # -v            --invert-match          Invertiert die Suche und liefert alle Zeilen die nicht auf das gesuchte Muster passen.
 # -f Datei      --file=Datei            beziehe die Muster aus Datei, eines je Zeile. Eine leere Datei enthält keine Muster und passt somit auf keinen String.
+
+now=$(date +"%T")
+echo "<$now> Finished Applying RegEx Blacklist"
+
 
 sudo rm -rf ./blocklist-work
