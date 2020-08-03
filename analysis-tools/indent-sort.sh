47d256068c869074aff06f92766891cf0634c16e
OlJohnny <Jonatwebhan@gmail.com>
2020-08-03 18:45:52 +0200

    updated analysis tools

--- analysis-tools/indent-sort.sh
+++ analysis-tools/indent-sort.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# github.com/OlJohnny | 2019
+# github.com/OlJohnny | 2020
 set -o errexit
 set -o pipefail
 set -o nounset
@@ -8,8 +8,7 @@ set -o nounset
 
 ### reset files ###
 rm -f ./.blocklist-fin*
-rm -f ./blocklist-rev
-touch ./blocklist-rev
+rm -f ./.blocklist-2dots*
 
 
 ### reverse and sort the list ###
@@ -17,10 +16,11 @@ read -p $'\e[96mMake backwards sorted and indented blocklist (this can take up t
 if [[ "${var1}" == "y" ]]
 then
 	echo -e "\n<$(date +"%T")> Sorting and Cleaning Blocklist..."
-	rev ./blocklist-fin > ./.blocklist-fin0
-	sort --unique ./.blocklist-fin0 --output=./.blocklist-fin1
-	sed --in-place '/^[[:space:]]*$/d' ./.blocklist-fin1
-	rev ./.blocklist-fin1 > ./.blocklist-fin2
+	rm -f ./.blocklist-rev.txt
+	rev ./blocklist-fin.txt > ./.blocklist-fin0.txt
+	sort --unique ./.blocklist-fin0.txt --output=./.blocklist-fin1.txt
+	sed --in-place '/^[[:space:]]*$/d' ./.blocklist-fin1.txt
+	rev ./.blocklist-fin1.txt > ./.blocklist-fin2.txt
 
 	echo -e "\n<$(date +"%T")> Indenting Blocklist..."
 	while read -r line
@@ -34,22 +34,21 @@ then
         do
                 line=$(sed 's/^/  /' <<< $line)
         done
-		echo "$line" >> ./blocklist-rev
+		echo "$line" >> ./blocklist-rev.txt
 	done < "./.blocklist-fin2"
 fi
 
 
 ### find most common domain.tld ###
 echo -e "\n<$(date +"%T")> Finding most common 'domain.tld'..."
-rev ../blocklist-fin | sort --unique --output=./.blocklist-2dots0
-awk -F "." '{ print $1,$2 }' ./.blocklist-2dots0 > ./.blocklist-2dots1
-rev ./.blocklist-2dots1 > ./.blocklist-2dots2
-sed --in-place 's/ /\./g' ./.blocklist-2dots2
-uniq -c ./.blocklist-2dots2 | sort --ignore-leading-blanks --general-numeric-sort --reverse | grep --extended-regexp --invert-match --file=./2dots-whitelist > ./blocklist-2dots
+rev ../blocklist-fin.txt | sort --unique --output=./.blocklist-2dots0.txt
+awk -F "." '{ print $1,$2 }' ./.blocklist-2dots0.txt > ./.blocklist-2dots1.txt
+rev ./.blocklist-2dots1.txt > ./.blocklist-2dots2.txt
+sed --in-place 's/ /\./g' ./.blocklist-2dots2.txt
+uniq -c ./.blocklist-2dots2.txt | sort --ignore-leading-blanks --general-numeric-sort --reverse | grep --extended-regexp --invert-match --file=./2dots-whitelist.txt > ./blocklist-2dots.txt
 
 
 
-##### FINISHING #####
 ### reset files ###
 rm -f ./.blocklist-fin*
 rm -f ./.blocklist-2dots*
